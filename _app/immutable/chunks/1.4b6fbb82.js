4fdaac68ba89441ac23bdd7f95b21859bfc5028f
Joey Khalil <mokhito@users.noreply.github.com>
2023-11-25 11:44:37 +0100

    small fixes

--- _app/immutable/chunks/1.4b6fbb82.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.52316361.js";export{t as component};
--- _app/immutable/chunks/2.cb13d96e.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.f19c79c2.js";export{t as component};
