e85c268e7b1150a7b3c6f15489e5c4e23000bcc6
Joey Khalil <mokhito@users.noreply.github.com>
2024-03-01 17:54:19 +0100

    multiple fixes

---
+++ _app/immutable/chunks/1.353acdc7.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.b629ce8b.js";export{t as component};
--- _app/immutable/chunks/2.25906a56.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.f653995d.js";export{t as component};
